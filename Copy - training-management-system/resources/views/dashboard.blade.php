e3a0097e6c826e31595bef679c686b0c61df615f
capung <krisnabumir@gmail.com>
2025-02-05 14:19:07 +0700

    Update Banner and Company Model

--- Copy - training-management-system/resources/views/dashboard.blade.php
+++ Copy - training-management-system/resources/views/dashboard.blade.php
@@ -64,4 +64,11 @@
         </div>
     </div>
 </body>
+<script>
+    $(document).ready(function() {
+        $('#totalUsers').text({{ $totalUsers }});
+        $('#totalCompanies').text({{ $totalCompanies }});
+        $('#totalBanners').text({{ $totalBanners }});
+    });
+</script>
 </html>
--- training-management-system/app/Http/Controllers/DashboardController.php
+++ training-management-system/app/Http/Controllers/DashboardController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\Banner;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(){
+        $totalUsers = user::count();
+        $totalCompanies = user::count();
+        $totalBanners = user::count();
+
+        return view('dashboard', compact('totalUsers', 'totalCompanies', 'totalBanners'));
+    }
 }
