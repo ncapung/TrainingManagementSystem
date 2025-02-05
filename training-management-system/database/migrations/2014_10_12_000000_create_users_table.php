5e71be5ccf0d50869a6a0b48c592bafba07c79fd
capung <krisnabumir@gmail.com>
2025-02-05 09:00:52 +0700

    Fix user hashed password - UserSeeder.php

--- training-management-system/database/migrations/2014_10_12_000000_create_users_table.php
+++ training-management-system/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number')->nullable();
+            $table->string('company')->nullable();
+            $table->string('role');
+            $table->date('birthday')->nullable();
             $table->timestamps();
         });
     }
--- training-management-system/database/seeders/UserSeeder.php
+++ training-management-system/database/seeders/UserSeeder.php
@@ -1,34 +1,26 @@
 <?php
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
+User::create([
+    'username' => 'admin',
+    'name' => 'Administrator',
+    'email' => 'admin@gmail.com',
+    'password' => Hash::make('adminnimda'),
+    'phone_number' => '08123456789',
+    'company' => 'AdminCorp.',
+    'role' => 'admin',
+    'birthday' => '2022-06-22',
+]);
 
-class UserSeeder extends Seeder{
-    public function run(){
-        User::create([
-            'username' => 'admin',
-            'name' => 'Administrator',
-            'email' => 'admin@gmail.com',
-            'password' => 'adminnimda',
-            'phone_number' => '08123456789',
-            'company' => 'AdminCorp.',
-            'role' => 'admin',
-            'birthday' => '2022-06-22',
-        ]);
-
-        User::create([
-            'username' => 'guest',
-            'name' => 'Guest user',
-            'email' => 'guest@gmail.com',
-            'password' => 'guesttseug',
-            'phone_number' => '0987654321',
-            'company' => 'GuestCorp.',
-            'role' => 'guest',
-            'birthday' => '2002-02-02',
-        ]);
-    }
-}
+User::create([
+    'username' => 'guest',
+    'name' => 'Guest user',
+    'email' => 'guest@gmail.com',
+    'password' => Hash::make('guesttseug'),
+    'phone_number' => '0987654321',
+    'company' => 'GuestCorp.',
+    'role' => 'guest',
+    'birthday' => '2002-02-02',
+]);
